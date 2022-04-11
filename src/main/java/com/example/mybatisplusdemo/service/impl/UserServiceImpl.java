3b4d58a0e366fc134d813139782a2e1dfc89cf35
fangzheng <18843426103@163.com>
2022-04-11 17:23:38 +0800

    serviceImpl中使用baseMapper替代特定的Mapper

--- src/main/java/com/example/mybatisplusdemo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/mybatisplusdemo/service/impl/UserServiceImpl.java
@@ -6,20 +6,15 @@ import com.example.mybatisplusdemo.mapper.UserMapper;
 import com.example.mybatisplusdemo.service.UserService;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.Resource;
-
 /**
  * @author fangzheng
  */
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements UserService {
 
-    @Resource
-    private UserMapper userMapper;
-
     @Override
     public User selectAllByName(String userName) {
-        return userMapper.selectAllByName(userName);
+        return baseMapper.selectAllByName(userName);
     }
 
 
